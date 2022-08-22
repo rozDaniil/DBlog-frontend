a57e0221ba756b2c45b76e9b4397f79f9ef593a4
unknown <rozenberg@machaon.ru>
2022-08-22 20:21:13 +0300

    fix3

--- src/pages/AddPost.tsx
+++ src/pages/AddPost.tsx
@@ -64,7 +64,7 @@ export const AddPost = () => {
   };
 
   const onRemoveImage = () => {
-    setPost((prev) => ({ ...prev, imageURL: null }));
+    setPost((prev) => ({ ...prev, imageUrl: null }));
   };
 
   const onTextChange = React.useCallback((value: string) => {
